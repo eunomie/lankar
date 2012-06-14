e95e0f71b39a7903c5f721c828da3cb860a3444c
CrEv <yves.brissaud@gmail.com>
2012-06-14 17:40:00 +0200

    add samples

--- lib/lankar/src/Lankar/LinksCollection.php
+++ lib/lankar/src/Lankar/LinksCollection.php
@@ -14,7 +14,9 @@ class LinksCollection {
   public function get() {
     return array(
       '1' => new Link(1, 'http://www.winsos.net/~yves'),
-      '2' => new Link(2, 'http://twitter.com/_crev_')
+      '2' => new Link(2, 'http://twitter.com/_crev_'),
+      '3' => new Link(3, 'http://n.survol.fr/n/document-store-a-recommander?utm_source=dlvr.it&utm_medium=twitter&utm_campaign=document-store-a-recommander'),
+      '4' => new Link(4, 'http://www.usinenouvelle.com/article/fleur-pellerin-dans-les-allees-de-futur-en-seine.N176704#xtor=RSS-215')
     );
   }
 }
