004966a1b11e78eba44d178c306160a5b13d2df0
CrEv <yves.brissaud@gmail.com>
2012-06-15 23:49:57 +0200

    move config and try markdown

--- web/pagedown.js
+++ web/pagedown.js
@@ -1,11 +1,34 @@
-// based on http://blog.angularjs.org/2012/05/custom-components-part-1.html
-angular.module('lankar', []).directive('markdown', function() {
-	var converter = new Markdown.Converter();
-	return {
-		restrict: 'E',
-		link: function(scope, element, attrs) {
-			var htmlText = converter.makeHtml(element.text());
-			element.html(htmlText);
-		}
-	};
+angular.module('lankar', [], function($compileProvider) {
+	$compileProvider.directive('markdown', function($compile) {
+		var converter = new Markdown.Converter();
+		return {
+			restrict: 'E',
+			link: function(scope, element, attrs) {
+//				$compile(element.contents())(scope);
+			}
+		};
+	});
 });
+
+function LinksCtrl($scope) {
+	$scope.links = [
+		{
+			para:[
+				'Ceci **est** simplement mon blog personnel. Je le met à jour de manière assez irrégulière, même si ça commence à redevenir suffisamment fréquent.',
+				'Pour info le premier billet date de septembre 2005'
+			],
+			url: 'http://www.winsos.net/~yves',
+			labels: ['next', 'perso'],
+			date: '15 juin 2012',
+			hash: '-BymHg'
+		}, {
+			para: [
+				'Mon compte twitter. Pas grand chose dessus, je m\'en sert pas mal pour suivre des gens. Disons que je m\'y met petit à petit.'
+			],
+			url: 'http://twitter.com/_crev_',
+			labels: ['next', 'perso', 'twitter'],
+			date: '15 juin 2012',
+			hash:'1dIdrA'
+		}
+	];
+}
