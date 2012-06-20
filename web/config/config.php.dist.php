a213f0d14e4421ad2b24e97502541d7850d29576
CrEv <yves.brissaud@gmail.com>
2012-06-20 16:35:51 +0200

    Configure and provide doctrine

---
+++ web/config/config.php.dist.php
@@ -0,0 +1,10 @@
+<?php
+
+// Data base configuration
+$lankar_db_options = array(
+  'driver' => '',
+  'host' => '',
+  'user' => '',
+  'password' => '',
+  'dbname' => 'lankar'
+);
--- web/index.php
+++ web/index.php
@@ -2,12 +2,17 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
+require_once __DIR__.'/config/config.php';
+
 $app = new Silex\Application();
 
 $app['debug'] = true;
 
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
   'twig.path' => __DIR__.'/../views',
+));
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+  'db.options' => $lankar_db_options
 ));
 $app->mount('/links', new Lankar\LinksControllerProvider());
 
