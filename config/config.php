933ca53f3be4f77307689ed15b62682be848857e
CrEv <yves.brissaud@gmail.com>
2012-06-20 16:46:54 +0200

    Move config file

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+// Data base configuration
+$lankar_db_options = array(
+  'driver' => 'pdo_pgsql',
+  'host' => 'localhost',
+  'user' => 'postgres',
+  'password' => 'postgres',
+  'dbname' => 'lankar'
+);
