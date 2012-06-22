5d4eb9f1d0e63b2d10483a72285b1658ef0631da
CrEv <yves.brissaud@gmail.com>
2012-06-22 11:02:46 +0200

    links db management
    Allow to store links, go to a link, get link details, get links (with
    pagination)
    Rename dblinks to links

--- lib/lankar/src/Lankar/LinksControllerProvider.php
+++ lib/lankar/src/Lankar/LinksControllerProvider.php
@@ -17,18 +17,44 @@ class LinksControllerProvider implements ControllerProviderInterface {
     $controllers = $app['controllers_factory'];
 
     $controllers->get('/links/{pagenumber}', function(Request $request) use ($app) {
-      $pagenumber = $request->attributes->get('name');
-      $content = LinksCollection::getCollection()->getJson();
-      return $app->json($content);
-    });
-
-    $controllers->get('/dblinks/{pagenumber}', function(Request $request) use ($app) {
-      $links = $app['db']->fetchAll('SELECT "id", "url", "title", "hash", "desc", "date" from links');
+      $pagenumber = (int)$request->attributes->get('pagenumber');
+      $query = 'SELECT "id" from links'; // order by date
+      $res = $app['db']->fetchAll($query);
+      $count = count($res);
+      $start = min(($pagenumber - 1) * 10, $count);
+      $stop = min($pagenumber * 10 + 1, $count);
+      $linksid = array();
+      for ($i = $start; $i < $stop; $i++) {
+        array_push($linksid, intval($res[$i]['id']));
+      }
+      $stmt = $app['db']->executeQuery('SELECT "id", "url", "title", "hash", "desc", "date" from links where "id" in (?)', array($linksid), array(\Doctrine\DBAL\Connection::PARAM_INT_ARRAY));
+      $links = $stmt->fetchAll();
       return $app->json(array(
         'links' => $links,
-        'pagenumber' => 1,
-        'total' => 2
+        'pagenumber' => $pagenumber,
+        'total' => count($count / 10)
       ));
+    });
+
+    $controllers->get('/go/{hash}', function(Request $request) use ($app) {
+      $hash = $request->attributes->get('hash');
+      $query = 'SELECT "url" from links where "hash" = ?';
+      $link = $app['db']->fetchAssoc($query, array($hash));
+
+      return $app->redirect($link['url']);
+    });
+
+    $controllers->get('/link/{hashOrUrl}', function(Request $request) use ($app) {
+      $hashOrUrl = $request->attributes->get('hashOrUrl');
+      $query = 'SELECT "id", "url", "title", "hash", "desc", "date" from links';
+      if (strlen($hashOrUrl) == 6 && 'http' != substr($hashOrUrl, 0, strlen('http'))) {
+        $query .= ' where "hash" = ?';
+      } else {
+        // need to decode URL
+        $query .= ' where "url" = ?';
+      }
+      $link = $app['db']->fetchAssoc($query, array($hashOrUrl));
+      return $app->json($link);
     });
 
     $controllers->post('/link', function(Request $request) use ($app) {
@@ -51,8 +77,14 @@ class LinksControllerProvider implements ControllerProviderInterface {
       }
       $date = date("d F Y H:m:s");
       $link = new Link($url, $title, $desc, $labels, $date);
-      print_r($link);
-      // save
+      $arr = $link->asArray();
+      $app['db']->insert('links', array(
+        '"url"'    => $arr['url'],
+        '"title"'  => $arr['title'],
+        '"hash"'   => $arr['hash'],
+        '"desc"'   => $arr['desc'],
+        '"hash"'   => $arr['hash']
+      ));
 
       return new Response('Link '.$link->hash().' created', 201);
     });
